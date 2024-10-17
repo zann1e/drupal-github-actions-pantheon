922365e9741578d87a06f9d3f3200d9ab8f48cf0
Alex Zani <zani.alexandr@gmail.com>
2024-10-17 19:10:31 +0300

    Sync code.

--- web/private/quicksilver/sync_code.php
+++ web/private/quicksilver/sync_code.php
@@ -1,9 +1,9 @@
 <?php
-
-// Database updates.
-echo "Running database updates.\n";
-passthru('drush -y updb');
-
 // Import all config changes.
 echo "Importing configuration from yml files...\n";
-passthru('drush -y config-import');
+passthru('drush config-import -y');
+echo "Import of configuration complete.\n";
+//Clear all cache
+echo "Rebuilding cache.\n";
+passthru('drush cr');
+echo "Rebuilding cache complete.\n";
